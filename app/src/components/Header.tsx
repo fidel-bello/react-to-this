e2daae4b9101b223dc99a68f9989daab6fc92577
Fidel <59423@bears.smcc.edu>
2023-05-05 23:35:18 -0400

    header component

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Stepper, Button, Group } from "@mantine/core";
+import UploadImage from "./UploadImage";
+import ImageFilter from "./ImageFilter";
+import RenderImage from "./RenderImage";
+import DownloadImage from "./Downloadmage";
+
+const Header: React.FC = (): JSX.Element => {
+  const [active, setActive] = useState(1);
+
+  const next = () => setActive((curr) => (curr < 3 ? curr + 1 : curr));
+  const prev = () => setActive((curr) => (curr > 0 ? curr - 1 : curr));
+
+  return (
+    <>
+      <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+        <Stepper.Step label="First step" description="Add an Image">
+         <UploadImage />
+        </Stepper.Step>
+        <Stepper.Step label="Second step" description="Verify email">
+          <ImageFilter />
+        </Stepper.Step>
+        <Stepper.Step label="Final step" description="Get full access">
+          <RenderImage />
+        </Stepper.Step>
+        <Stepper.Completed>
+          <DownloadImage />
+        </Stepper.Completed>
+      </Stepper>
+      <Group position="center" mt="xl">
+        <Button variant="default" onClick={prev}>
+          Back
+        </Button>
+        <Button onClick={next}>Next step</Button>
+      </Group>
+    </>
+  );
+};
+
+export default Header;

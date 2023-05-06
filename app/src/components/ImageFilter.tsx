4dc45eb92dfdbfc393288d00794ee398417d1f0f
Fidel <59423@bears.smcc.edu>
2023-05-05 23:35:38 -0400

    image filter component

---
+++ app/src/components/ImageFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageFilter: React.FC = (): JSX.Element => {
+    return <><div>We will select a type of filter for this image</div></>
+}
+
+export default ImageFilter;

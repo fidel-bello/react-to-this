be1b7532b18625093c75cd7832479e8b08803349
Fidel <59423@bears.smcc.edu>
2023-05-05 23:35:00 -0400

    upload img component

---
+++ app/src/components/UploadImage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UploadImage: React.FC = (): JSX.Element => {
+    return <><div>We will create an image in this components</div></>
+}
+
+export default UploadImage;
